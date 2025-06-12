48f49b2b9ce45a9d001d67a2ad4bedabe53ca4d1
Joyce-Nkengbeza <Joyce.nkengbeza@stu.ocsb.ca>
2025-06-12 01:45:28 +0000

    added

--- answer.php
+++ answer.php
@@ -1,36 +1,21 @@
 <!DOCTYPE html>
-<!-- ICS2O-Unit5-01PHP-HTML-MDL -->
+<!-- ICD2O-Unit 6-03-PHP -->
 <html lang="en-ca">
 
 <head>
   <meta charset="utf-8" />
-  <meta name="description" content="Random Number Guess, PHP" />
+  <meta name="description" content="Random Coffee Image, in PHP" />
   <meta name="keywords" content="mths, icd2o" />
   <meta name="author" content="Joyce Nkengbeza" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link
-    rel="stylesheet"
-    href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-  <link
-    rel="stylesheet"
-    href="https://code.getmdl.io/1.3.0/material.deep_purple-purple.min.css" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.deep_purple-pink.min.css" />
   <link rel="stylesheet" href="./css/style.css" />
-  <link
-    rel="apple-touch-icon"
-    sizes="180x180"
-    href="./apple-touch-iconnew.png" />
-  <link
-    rel="icon"
-    type="image/png"
-    sizes="32x32"
-    href="./favicon-32x32new.png" />
-  <link
-    rel="icon"
-    type="image/png"
-    sizes="16x16"
-    href="./favicon-16x16new.png" />
+  <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
   <link rel="manifest" href="./site.webmanifest" />
-  <title>Random Number Guess, PHP</title>
+  <title>Random Coffee Image, in PHP</title>
 </head>
 
 <body>
@@ -38,37 +23,43 @@
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Random Number Guess, in PHP</span>
+        <span class="mdl-layout-title">Random Coffee Image, in PHP</span>
       </div>
     </header>
     <main class="mdl-layout__content">
-      <div class="right-image">
-        <img src="./images/dice.gif" />
+
+      <?php
+      // API URL for random coffee image
+      $apiUrl = "https://coffee.alexflipnote.dev/random.json";
+
+      // Fetch coffee image data
+      $resultJSON = file_get_contents($apiUrl);
+
+      if ($resultJSON === FALSE) {
+        // Error if fetch fails
+        echo "<div class='page-content'>Sorry, an error has occurred. Please try again later.</div>";
+        return;
+      }
+
+      // Decode JSON data
+      $jsonData = json_decode($resultJSON, true);
+
+      // Get coffee image URL
+      $coffeeImageUrl = $jsonData['file'];
+
+      // Output image
+      echo "<div class='page-content'>";
+      echo "<p>Enjoy your randomly brewed coffee ☕</p>";
+      echo "<img src='" . $coffeeImageUrl . "' alt='Random Coffee Image'>";
+      echo "</div>";
+      ?>
+
+      <br />
+      <div class="page-content-answer">
+        <a href="./index.php">Return ...</a>
       </div>
-      <div class="page-content-php">
-        <div id="user-info">
-          <?php
-          $randomNumber = random_int(1, 6);
-          //input
-          $userNumber = (int) $_GET["userNumber"];
-          // process
-          if ($userNumber === $randomNumber) {
-            // output
-            echo "The number " . $userNumber . " was the correct number";
-          }
-          // process
-          if ($userNumber === $randomNumber) {
-            echo "You have guessed the correct number!";
-          } else {
-            echo "You have guessed the wrong number!<br/>The Correct Number Was: " . $randomNumber;
-          }
-          ?>
-          <div class="page-content-answer">
-            <a href="./index.php">Return</a>
-          </div>
-        </div>
     </main>
   </div>
-  </body>
+</body>
 
 </html>
--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<!-- ICS2O-Unit5-01PHP-HTML-MDL -->
+<!-- ICS2O-Assignment-06-PHP-HTML-MDL -->
 <html lang="en-ca">
 
 <head>
   <meta charset="utf-8" />
-  <meta name="description" content="Random Number Guess, PHP" />
+  <meta name="description" content="Random Coffee Image, PHP" />
   <meta name="keywords" content="mths, icd2o" />
   <meta name="author" content="Joyce Nkengbeza" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -30,38 +30,28 @@
     sizes="16x16"
     href="./favicon-16x16new.png" />
   <link rel="manifest" href="./site.webmanifest" />
-  <title>Random Number Guess, PHP</title>
+  <title>Random Coffee Image, PHP</title>
 </head>
-
 
 <body>
   <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-  <script src="./js/script.js"></script>
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">Random Number Guess, in PHP</span>
+        <span class="mdl-layout-title">Random Coffee Image, PHP</span>
       </div>
     </header>
     <main class="mdl-layout__content">
-      <div class="right-image">
-        <img src="./images/dice.gif" alt="dice gif" />
-      </div>
-      <div class="page-content">Pick a number between 1 and 6</div>
+      <div class="page-content">Click button to get a random coffee image.</div>
       <br />
       <br />
       <div class="page-content-php">
-        <form action="./answer.php" method="GET">
-          <div class="mdl-textfield mdl-js-textfield">
-            <input class="mdl-textfield__input" type="number" name="userNumber" />
-            <label class="mdl-textfield__label" for="userNumber">enter</label>
-            <span class="mdl-textfield__error">Input is not a number!</span>
-          </div>
-          <br />
+        <form method="POST" action="answer.php">
           <!-- Accent-colored raised button with ripple -->
-          <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-            type="submit">
-            Check!
+          <button
+            class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
+            type="submit" name="getCoffee">
+            Get Coffee Image
           </button>
         </form>
       </div>
